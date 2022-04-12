6b9514513751eb54be15330b41de19615db9748a
Nick M <mully7773@gmail.com>
2022-04-12 15:23:56 -0500

    Added some styling to the paper cards

--- frontend/src/components/NavigationCard/index.js
+++ frontend/src/components/NavigationCard/index.js
@@ -1,20 +1,21 @@
 import { Paper, Container } from "@mui/material";
 import { Grid, Item} from "@mui/material";
 
+import "./navigationCard.css";
 
 const NavigationCard = () => {
     return ( 
      
-    <Container>
-    <Grid container spacing={3}>
+    <Container sx={{ padding: 3}}>
+    <Grid container spacing={10}>
     <Grid item xs={6}>
         
-    <Paper elevation={3}>Men</Paper> 
+    <Paper className="navCard" sx={{ padding: 2, border: 1, fontSize: 18}} elevation={3} variant="outlined">SHOP MEN'S</Paper> 
     
     </Grid>
     <Grid item xs={6}>
         
-    <Paper elevation={3}>Women</Paper> 
+    <Paper className="navCard" sx={{ padding: 2, border: 1, fontSize: 18}} elevation={3} variant="outlined">SHOP WOMEN'S</Paper> 
     
     </Grid>
         
