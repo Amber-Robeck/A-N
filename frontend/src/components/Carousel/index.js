62cb86ffc2d8a4994dfc3103c7d4bbd1bdce7f25
Nick M <mully7773@gmail.com>
2022-04-14 15:32:51 -0500

    added text to carousel

--- frontend/src/components/Carousel/index.js
+++ frontend/src/components/Carousel/index.js
@@ -43,10 +43,10 @@ export default function Carousel() {
         effect={"fade"}
         centeredSlides={true}
         rewind={true}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
+        // autoplay={{
+        //   delay: 5000,
+        //   disableOnInteraction: false,
+        // }}
         pagination={{
           clickable: true,
         }}
@@ -54,8 +54,21 @@ export default function Carousel() {
         modules={[Autoplay, EffectFade, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide><img src={`${process.env.PUBLIC_URL}/assets/images/jannis-lucas-P0i2_PmGtqI-unsplash.jpg`} alt="Man and shirt"/></SwiperSlide>
-        <SwiperSlide><img src={`${process.env.PUBLIC_URL}/assets/images/matheus-ferrero-4QotHf68gSg-unsplash.jpg`} alt="Woman and denim shirt"/></SwiperSlide>
+        <SwiperSlide className="slide-one"><span className="carousel-text">
+          <b>35% off</b>
+          <br></br>
+          relaxing comfort
+          <br></br>
+          from sunrise to sunset
+          </span></SwiperSlide>
+        <SwiperSlide className="slide-two"><span className="carousel-text">
+        <b>denim</b>
+        <br></br>
+        cool and stylish
+        <br></br>
+        shop for spring
+        </span></SwiperSlide>
+        
         <SwiperSlide><img src={`${process.env.PUBLIC_URL}/assets/images/xuan-thu-le-2OXNxfTt3kQ-unsplash.jpg`} alt="Man and woman with stylish outfits"/></SwiperSlide>
       </Swiper>
     </>
