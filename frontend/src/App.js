bffcd57c01cf203826b3e87c6911844d791ec81d
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-04-14 13:27:59 -0500

    saved merging issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,14 @@
-<<<<<<< HEAD
-// import './App.css';
-=======
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
->>>>>>> 3dc0ffd200caef32d8c74be159e46a9aea945316
 
 // //components:
 // import NavBar from './components/NavBar';
 
-<<<<<<< HEAD
-// function App() {
-//   return (
-//     <div className="App">
-//       <NavBar/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import "./App.css";
-import Auth from "./components/Auth/index.js";
-import Layout from "./components/Layout";
-import Notification from "./components/Notification";
-import { fetchData, sendCartData } from "./store/cart-actions";
-import { uiActions } from "./store/ui-slice";
-let isFirstRender = true;
-=======
 //pages:
 import Home from './pages/Home';
 import Mens from './pages/Men';
 import Womens from './pages/Women';
 
->>>>>>> 3dc0ffd200caef32d8c74be159e46a9aea945316
 function App() {
   const dispatch = useDispatch();
   const notification = useSelector((state) => state.ui.notification);
@@ -55,24 +29,14 @@ function App() {
     }
   }, [cart, dispatch]);
   return (
-<<<<<<< HEAD
-    <div className="App">
-      {notification && (
-        <Notification type={notification.type} message={notification.message} />
-      )}
-      {!isLoggedIn && <Auth />}
-      {isLoggedIn && <Layout />}
-    </div>
-=======
     <BrowserRouter>
-    <NavBar/>
-    <Routes>
-     <Route path="/" element={<Home/>}/>
-     <Route path="/men" element={<Mens/>}/>
-     <Route path="/women" element={<Womens/>}/>
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/men" element={<Mens />} />
+        <Route path="/women" element={<Womens />} />
+      </Routes>
     </BrowserRouter>
->>>>>>> 3dc0ffd200caef32d8c74be159e46a9aea945316
   );
 }
 
