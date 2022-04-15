56b3b1449f5acfca58c163207d7152179364b738
Nick M <mully7773@gmail.com>
2022-04-15 16:02:52 -0500

    added text on last carousel slide and cleaned up the code

--- frontend/src/components/Carousel/index.js
+++ frontend/src/components/Carousel/index.js
@@ -1,26 +1,3 @@
-// import { Swiper, SwiperSlide } from 'swiper/react';
-
-// // Import Swiper styles
-// import 'swiper/css';
-// import 'swiper/css/effect-fade';
-
-// const Carousel = () => {
-//     return ( <Swiper
-//         spaceBetween={50}
-//         slidesPerView={3}
-//         onSlideChange={() => console.log('slide change')}
-//         onSwiper={(swiper) => console.log(swiper)}
-//       >
-//         <SwiperSlide>Slide 1</SwiperSlide>
-//         <SwiperSlide>Slide 2</SwiperSlide>
-//         <SwiperSlide>Slide 3</SwiperSlide>
-//       </Swiper> );
-// }
- 
-// export default Carousel;
-
-
-import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -43,10 +20,10 @@ export default function Carousel() {
         effect={"fade"}
         centeredSlides={true}
         rewind={true}
-        // autoplay={{
-        //   delay: 5000,
-        //   disableOnInteraction: false,
-        // }}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
         pagination={{
           clickable: true,
         }}
@@ -60,16 +37,23 @@ export default function Carousel() {
           relaxing comfort
           <br></br>
           from sunrise to sunset
-          </span></SwiperSlide>
-        <SwiperSlide className="slide-two"><span className="carousel-text">
-        <b>denim</b>
-        <br></br>
-        cool and stylish
-        <br></br>
-        shop for spring
         </span></SwiperSlide>
         
-        <SwiperSlide><img src={`${process.env.PUBLIC_URL}/assets/images/xuan-thu-le-2OXNxfTt3kQ-unsplash.jpg`} alt="Man and woman with stylish outfits"/></SwiperSlide>
+        <SwiperSlide className="slide-two"><span className="carousel-text">
+          <b>denim</b>
+          <br></br>
+          cool and stylish
+          <br></br>
+          shop for spring
+        </span></SwiperSlide>
+
+        <SwiperSlide className="slide-three"><span className="carousel-text">
+          <b>just arrived</b>
+          <br></br>
+          the comfort of home
+          <br></br>
+          every day
+        </span></SwiperSlide>
       </Swiper>
     </>
   );
