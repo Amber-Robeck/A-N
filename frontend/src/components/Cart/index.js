1f591b968ccfd5e4468a64e6d41e3f5e2666e4a1
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-04-14 19:32:49 -0500

    deleted unused code for inline styling

--- frontend/src/components/Cart/index.js
+++ frontend/src/components/Cart/index.js
@@ -12,17 +12,7 @@ const Cart = () => {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    // const style = {
-    //     position: 'absolute',
-    //     top: '50%',
-    //     left: '50%',
-    //     transform: 'translate(-50%, -50%)',
-    //     width: '90%',
-    //     bgcolor: 'background.paper',
-    //     border: '2px solid #000',
-    //     boxShadow: 24,
-    //     p: 4,
-    // }
+
     return (
         <div className="cartIcon">
             {/* <h3 onClick={showCart}>Cart: {quantity} Items</h3> */}
