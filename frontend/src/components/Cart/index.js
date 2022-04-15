8d687a3d8e5bea55e823618af6ecb3bb63497bdf
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-04-14 19:32:16 -0500

    removed inline styling from modal on Cart.css

--- frontend/src/components/Cart/index.js
+++ frontend/src/components/Cart/index.js
@@ -12,17 +12,17 @@ const Cart = () => {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: '90%',
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-    }
+    // const style = {
+    //     position: 'absolute',
+    //     top: '50%',
+    //     left: '50%',
+    //     transform: 'translate(-50%, -50%)',
+    //     width: '90%',
+    //     bgcolor: 'background.paper',
+    //     border: '2px solid #000',
+    //     boxShadow: 24,
+    //     p: 4,
+    // }
     return (
         <div className="cartIcon">
             {/* <h3 onClick={showCart}>Cart: {quantity} Items</h3> */}
@@ -33,7 +33,7 @@ const Cart = () => {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box sx={style}>
+                <Box className="cartMdl">
                     <Button onClick={handleClose} className="closeBtn">X</Button>
                     <Typography id="modal-modal-title" variant="h6" component="h2">
                         Your cart
