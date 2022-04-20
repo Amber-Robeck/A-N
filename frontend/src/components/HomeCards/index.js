6d12adbe383066fadf99338dc3dbf3c8e4e781fc
Nick M <mully7773@gmail.com>
2022-04-20 14:36:27 -0500

    Added basic responsive function to home cards - not perfect

--- frontend/src/components/HomeCards/index.js
+++ frontend/src/components/HomeCards/index.js
@@ -1,49 +1,54 @@
 import { Grid, Box, Paper, Container } from "@mui/material";
 
 const HomeCards = () => {
-    return ( <Box>
-        <Container>
+            return (
+                <Container>
+        <Box sx={{ flexGrow: 1 }}>
         <Grid
-        container rowSpacing={4}
-        direction="row"
-        justifyContent="space-evenly"
-        alignItems="center"
-        marginTop={1}
+        container spacing={{ xs: 1, sm: 2, md: 3 }}
+        columnSpacing={{ xs: 1, sm: 2, md: 3}}
+        columns={{ xs: 1, sm: 8, md: 12 }}
+        // direction="row"
+        // justifyContent="space-evenly"
+        // alignItems="center"
+        // marginTop={1}
         >
-            <Grid item xs={4}>
-            <Paper elevation={1} sx={{height: '425px', width: '350px'}}>
+            <Grid item xs={2} sm={4} md={4}>
+            <Paper elevation={1} sx={{height: '425px', width: '350px', background: '#f8f8f8'}}>
+
             </Paper>
             </Grid>
 
-            <Grid item xs={4}>
-            <Paper elevation={1} sx={{height: '425px', width: '350px'}}>
+            <Grid item xs={2} sm={4} md={4}>
+            <Paper elevation={1} sx={{height: '425px', width: '350px', background: '#f8f8f8'}}>
             </Paper>
             </Grid>
 
-            <Grid item xs={4}>
-            <Paper elevation={1} sx={{height: '425px', width: '350px'}}>
+            <Grid item xs={2} sm={4} md={4}>
+            <Paper elevation={1} sx={{height: '425px', width: '350px', background: '#f8f8f8'}}>
             </Paper>
             </Grid>
 
-            <Grid item xs={4}>
-            <Paper elevation={1} sx={{height: '425px', width: '350px'}}>
+            <Grid item xs={2} sm={4} md={4}>
+            <Paper elevation={1} sx={{height: '425px', width: '350px', background: '#f8f8f8'}}>
             </Paper>
             </Grid>
 
-            <Grid item xs={4}>
-            <Paper elevation={1} sx={{height: '425px', width: '350px'}}>
+            <Grid item xs={2} sm={4} md={4}>
+            <Paper elevation={1} sx={{height: '425px', width: '350px', background: '#f8f8f8'}}>
             </Paper>
             </Grid>
 
-            <Grid item xs={4}>
-            <Paper elevation={1} sx={{height: '425px', width: '350px'}}>
+            <Grid item xs={2} sm={4} md={4}>
+            <Paper elevation={1} sx={{height: '425px', width: '350px', background: '#f8f8f8'}}>
             </Paper>
             </Grid>
 
 
         </Grid>
+    </Box>
         </Container>
-    </Box> );
+     );
 }
  
 export default HomeCards;
