106a3aa2bb1bf8c118412efd23b47678350def16
Nick M <mully7773@gmail.com>
2022-04-27 14:11:38 -0500

    Added server.js boilerplate code

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,32 @@
-//This will be the server file
+const express = require("express");
+const app = express();
+const path = require("path");
+const db = require('../backend/config/connection');
+const mongoose = require("mongoose");
+
+
+
+
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
+db.once('open', () => {
+    console.log('Mongoose DB connection established.')
+  });
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`🚀 Server ready at http://localhost:${port}`)
+});
