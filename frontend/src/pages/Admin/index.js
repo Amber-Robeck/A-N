daf23918cc6c5c8487a87c376d8f45beba25b945
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-05-11 22:24:12 -0500

    Created basic form in admin folder/productForm.js, commented out api call in admin while testing form

--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -1,48 +1,58 @@
 import React, { useEffect, useState } from 'react'
-// import ProductForm from './productForm';
+import ProductForm from './productForm';
 import { Grid, Typography } from '@mui/material'
 
+//Currently commented out api call to test product form
+
 const Admin = () => {
-    const API_URL = 'http://localhost:5000/admin'
-    const [products, setProducts] = useState([]);
-    const [fetchErr, setFetchErr] = useState(null);
-    useEffect(() => {
+    // const API_URL = 'http://localhost:5000/admin'
+    // const [products, setProducts] = useState([]);
+    // const [fetchErr, setFetchErr] = useState(null);
+    // useEffect(() => {
 
-        const fetchItems = async () => {
-            try {
-                const response = await fetch(API_URL);
-                if (!response.ok) throw Error('Did not recieve data');
-                const productList = await response.json();
-                console.log(productList);
-                setProducts(productList);
-            } catch (err) {
-                setFetchErr(err);
-            }
-        }
+    //     const fetchItems = async () => {
+    //         try {
+    //             const response = await fetch(API_URL);
+    //             if (!response.ok) throw Error('Did not recieve data');
+    //             const productList = await response.json();
+    //             console.log(productList);
+    //             setProducts(productList);
+    //         } catch (err) {
+    //             setFetchErr(err);
+    //         }
+    //     }
 
-        fetchItems();
-    }, []);
+    //     fetchItems();
+    // }, []);
+    // return (<>
+    //     <Grid sx={{ mt: 20 }}>
+    //         <Typography variant="h1" verticalAlign={'baseline'} textAlign={'center'} fontWeight={300}
+    //             marginBottom={'1em'} marginTop={'1em'} color='#000' fontSize={'32px'} letterSpacing={1.2}>
+    //             This is the admin page
+    //         </Typography>
+    //         {fetchErr &&
+    //             <Typography variant="h2" verticalAlign={'baseline'} textAlign={'center'} fontWeight={300}
+    //                 marginBottom={'1em'} marginTop={'1em'} color='#FF0000' fontSize={'32px'} letterSpacing={1.2}>
+    //                 {`Uh oh there was a problem! ${fetchErr}`}
+    //             </Typography>}
+    //         {products.map((product =>
+    //             <Typography key={product._id} variant="h3" textAlign={'center'} fontWeight={300}
+    //                 marginBottom={'1em'} marginTop={'1em'} color='#000' fontSize={'32px'} letterSpacing={1.2}>
+    //                 {product.name}
+    //                 <br />
+    //                 {product.description}
+    //             </Typography>
+    //         ))}
+
+    //     </Grid>
+    //     <Grid>
+
+    //         <ProductForm />
+    //     </Grid>
+    // </>
+    // )
     return (
-        <Grid sx={{ mt: 20 }}>
-            <Typography variant="h1" verticalAlign={'baseline'} textAlign={'center'} fontWeight={300}
-                marginBottom={'1em'} marginTop={'1em'} color='#000' fontSize={'32px'} letterSpacing={1.2}>
-                This is the admin page
-            </Typography>
-            {fetchErr &&
-                <Typography variant="h2" verticalAlign={'baseline'} textAlign={'center'} fontWeight={300}
-                    marginBottom={'1em'} marginTop={'1em'} color='#FF0000' fontSize={'32px'} letterSpacing={1.2}>
-                    {`Uh oh there was a problem! ${fetchErr}`}
-                </Typography>}
-            {products.map((product =>
-                <Typography key={product._id} variant="h3" textAlign={'center'} fontWeight={300}
-                    marginBottom={'1em'} marginTop={'1em'} color='#000' fontSize={'32px'} letterSpacing={1.2}>
-                    {product.name}
-                    <br />
-                    {product.description}
-                </Typography>
-            ))}
-
-        </Grid>
+        <ProductForm />
     )
 }
 
--- frontend/src/pages/Admin/productForm.js
+++ frontend/src/pages/Admin/productForm.js
@@ -1,18 +1,87 @@
-import { TextField, Grid } from '@mui/material'
+import { TextField, Grid, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, MenuItem, InputLabel, Select } from '@mui/material'
+import { height } from '@mui/system'
 import React, { useState, useEffect } from 'react'
+
+//This will be dynamic depending upon users choice, create, update
+//TODO: Bring data from api to prefill forms
+//TODO: Ternary for if onSale is true show/false hide
+//TODO: Add variants after data modeling is completed, checkbox for size input for number in inventory
+//TODO: Collect data from user and post/put
+//TODO: Complete on change for selects
+//TODO: Button for submit add/update
 
 const ProductForm = () => {
     return (
-        <form>
-            <Grid container sx={{ mt: 20 }}>
-                <Grid item xs={6}>
-                    <TextField
-                        variant='outlined'
-                        label='Product name' />
-                </Grid>
-                <Grid item xs={6}></Grid>
+        <Grid container sx={{ mt: 20 }} direction='column' alignItems='center'>
+            <Grid item xs={12}>
+                <TextField
+                    sx={{ m: 1, width: 300 }}
+                    variant='outlined'
+                    label='Product name' />
             </Grid>
-        </form>
+            <Grid item xs={12}>
+                <TextField
+                    sx={{ m: 1, width: 300 }}
+                    variant='outlined'
+                    label='Price of product'
+                    type='number' />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    sx={{ m: 1, width: 300 }}
+                    variant='outlined'
+                    multiline
+                    label='Product description' />
+            </Grid>
+            <Grid item xs={12}>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                    <InputLabel id="category">Category</InputLabel>
+                    <Select
+                        labelId="category"
+                        id="category"
+                        // defaultValue='Mens'
+                        label="Category"
+                    // onChange={handleChange}
+                    >
+                        {/* will need to add a value */}
+                        <MenuItem>Mens</MenuItem>
+                        <MenuItem>Womens</MenuItem>
+                        <MenuItem>Childrens</MenuItem>
+                    </Select>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                    <FormLabel id="onSale">On sale?</FormLabel>
+                    <RadioGroup
+                        row={true}
+                        aria-labelledby="onSale"
+                        defaultValue="false"
+                        name="radio-buttons-group"
+                    >
+                        <FormControlLabel value="true" control={<Radio />} label="Yes" />
+                        <FormControlLabel value="false" control={<Radio />} label="No" />
+                    </RadioGroup>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                    <InputLabel id="saleType">Sale type</InputLabel>
+                    <Select
+                        labelId="saleType"
+                        id="saleType"
+                        // defaultValue='get1now'
+                        label="saleType"
+                    // onChange={handleChange}
+                    >
+                        {/* will need to add a value */}
+                        <MenuItem>get1now</MenuItem>
+                        <MenuItem>save10</MenuItem>
+                        <MenuItem>newCustomer</MenuItem>
+                    </Select>
+                </FormControl>
+            </Grid>
+        </Grid>
     )
 }
 
