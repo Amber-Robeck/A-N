f0b9268b7bf25bececf478cb057bcdcdcde699df
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-05-11 23:04:42 -0500

    In Product.js added variationSchema, added as subdocument to ProductSchema under variations.

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 
 //TODO: Add quantity to schema, maybe within variations ie: [{black: [{s: 1}, {m: 2}, {l: 0}]}, {white: [{s: 0}, {m: 3}, {l: 4}]}]
+const variationSchema = new mongoose.Schema({
+    name: String,
+    availableSizes: Array,
+});
+
 const ProductSchema = mongoose.Schema({
     name: String,
     description: String,
@@ -10,8 +15,7 @@ const ProductSchema = mongoose.Schema({
     currentPrice: Number,
     onSale: Boolean,
     saleType: String,
-    variations: Array,
-    availableSizes: Array,
+    variations: [variationSchema],
 
 });
 
@@ -26,24 +30,33 @@ Product.find({}).exec((err, collection) => {
                     "name": "T-shirt",
                     "description": "Simple shirt made of cotton.",
                     "category": ["mens", "shirt"],
+                    "originalPrice": 15.99,
+                    "currentPrice": 14.99,
+                    "onSale": true,
+                    "saleType": "get1now",
+                    "variations": [{ "name": "gray", "availableSizes": [{ "s": 1 }, { "m": 2 }, { "xl": 1 }] }, { "name": "white", "availableSizes": [{ "s": 1 }, { "m": 7 }, { "xl": 4 }] }, { "name": "black", "availableSizes": [{ "s": 3 }, { "m": 1 }, { "xl": 2 }] }],
+                },
+                {
+                    "name": "Longsleeve shirt",
+                    "description": "Simple shirt made of cotton.",
+                    "category": ["mens", "shirt"],
                     "originalPrice": 14.99,
                     "currentPrice": 13.99,
                     "onSale": true,
                     "saleType": "get1now",
-                    "variations": ["gray", "black", "white"],
-                    "availableSizes": ["s", "m", "l"]
+                    "variations": [{ "name": "gray", "availableSizes": [{ "s": 1 }, { "m": 2 }, { "l": 0 }, { "xl": 1 }] }, { "name": "white", "availableSizes": [{ "s": 1 }, { "m": 7 }, { "xl": 4 }] }, { "name": "black", "availableSizes": [{ "s": 3 }, { "m": 1 }, { "xl": 2 }] }],
                 },
-                {
-                    "name": "T-shirt",
-                    "description": "Simple shirt made of cotton.",
-                    "category": ["womens", "shirt"],
-                    "originalPrice": 16.99,
-                    "currentPrice": 15.99,
-                    "onSale": true,
-                    "saleType": "get1now",
-                    "variations": ["gray", "black", "white"],
-                    "availableSizes": ["s", "m", "l"]
-                },
+                // {
+                //     "name": "T-shirt",
+                //     "description": "Simple shirt made of cotton.",
+                //     "category": ["womens", "shirt"],
+                //     "originalPrice": 16.99,
+                //     "currentPrice": 15.99,
+                //     "onSale": true,
+                //     "saleType": "get1now",
+                //     "variations": ["gray", "black", "white"],
+                //     "availableSizes": ["s", "m", "l"]
+                // },
 
             ],
             (insertErr) => {
